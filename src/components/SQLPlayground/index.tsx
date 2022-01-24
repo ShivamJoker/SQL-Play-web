6eebe31c5a49b73862a9a7c0e99690ef84d0baa3
Labham Jain <coding.labham@gmail.com>
2022-01-24 21:16:06 +0530

    :sparkles: Added intellisense support

--- src/components/SQLPlayground/index.tsx
+++ src/components/SQLPlayground/index.tsx
@@ -1,9 +1,44 @@
-import Editor, {EditorProps, useMonaco} from "@monaco-editor/react";
+import Editor, { useMonaco } from "@monaco-editor/react";
 import "@styles/SQLPlayground/index.scss";
+import sqlData from "@data/sql.json";
 import { useEffect } from "react";
 
 function SQLPlayground() {
   const monaco = useMonaco();
+  
+  function createDependencyProposals (range: any) {
+    return sqlData.map((item) => {
+      return {
+        kind: monaco.languages.CompletionItemKind.Function,
+        range: range,
+        insertTextRules: monaco?.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        insertText: item.label,
+        ...item,
+      }
+    })
+  };
+
+  useEffect(() => {
+    
+    monaco?.languages.registerCompletionItemProvider('sql', {
+
+      provideCompletionItems: (model:any, position: any) => {
+        const word = model.getWordUntilPosition(position);
+  
+        const range = {
+          startLineNumber: position.lineNumber,
+          endLineNumber: position.lineNumber,
+          startColumn: word.startColumn,
+          endColumn: word.endColumn
+        };
+  
+        return {
+          suggestions: createDependencyProposals(range)
+        }
+      }
+    });
+
+  }, [monaco])
 
   const onMount = (editor: any) => {
     editor.onKeyDown((event: any) => {
