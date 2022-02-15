67d3f086a9d8988100f82427414f8a6c83d34e80
Shivam <singhshivamkr@gmail.com>
2022-02-15 11:33:09 +0530

    :bug: Fix dynamic theming

--- src/components/Workspace/SearchBox/Accordian/CodeHighlighter.tsx
+++ src/components/Workspace/SearchBox/Accordian/CodeHighlighter.tsx
@@ -1,23 +1,28 @@
-import { motion } from 'framer-motion';
-import {PrismAsyncLight as SyntaxHighlighter} from 'react-syntax-highlighter';
+import { motion } from "framer-motion";
+import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
 
-import {materialLight, materialDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { IGlobalState } from '~types/global';
-
+import {
+  materialLight,
+  materialDark,
+} from "react-syntax-highlighter/dist/esm/styles/prism";
+import { IGlobalState } from "~types/global";
 
 interface Props {
-  text: string,
+  text: string;
   changeText: (text: string) => void;
-  theme: IGlobalState['theme']
+  theme: IGlobalState["theme"];
 }
-const CodeHighlighter: React.FC<Props> = ({text, changeText, theme}) => {
+const CodeHighlighter: React.FC<Props> = ({ text, changeText, theme }) => {
   return (
     <motion.div onClick={() => changeText(text)}>
-      <SyntaxHighlighter style={theme == "default" ? materialLight : materialDark} language="sql">
+      <SyntaxHighlighter
+        style={theme == "light" ? materialLight : materialDark}
+        language="sql"
+      >
         {text}
       </SyntaxHighlighter>
     </motion.div>
-  )
-}
+  );
+};
 
-export default CodeHighlighter
+export default CodeHighlighter;
