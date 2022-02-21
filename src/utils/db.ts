9091662c16a82fe4a4cc836d69763ddb162b66f0
Labham Jain <coding.labham@gmail.com>
2022-02-21 17:51:23 +0530

    :bug: Fix no table employees

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -6,15 +6,14 @@ let db: Database;
 const init = async (data?: Uint8Array) => {
   
   const sqlPromise = sqlJS({ locateFile: file => `https://sql.js.org/dist/${file}` });
-  
   if(!data){
     const dataPromise = fetch(import.meta.env.VITE_APP_DB_FILE).then(res => res.arrayBuffer());
     
     const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
-    return db = new SQL.Database(new Uint8Array(buf));
+    db = new SQL.Database(new Uint8Array(buf));
   } else {
     const SQL = await sqlPromise;
-    return db = new SQL.Database(data);
+    db = new SQL.Database(data);
   }
 }
 
@@ -22,10 +21,15 @@ const init = async (data?: Uint8Array) => {
 (async () => {
   const existingDBBlob: Blob | undefined = await get('database');
   const existingDatabase = await existingDBBlob?.arrayBuffer();
-  const data = new Uint8Array(existingDatabase as ArrayBufferLike);
-  init(data).then((db) => {
-    db = db;
-  });
+
+  const dbInitFunction = (db: void) => {
+     db = db;
+  }
+
+  if(existingDatabase){
+    const data = new Uint8Array(existingDatabase as ArrayBufferLike)
+    data ? init(data).then(dbInitFunction) : init().then(dbInitFunction);
+  }
 })()
 
 export const execCmd = (text:string) => db ? db.exec(text) : undefined;
@@ -35,5 +39,4 @@ export const saveDBState = () => {
   const database = db.export();
   const blob = new Blob([database]);
   set('database', blob);
-  console.log('saved')
 }
