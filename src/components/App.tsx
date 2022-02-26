96de6a6c2400145f5043f9a16af26d948b5f9eb3
Shivam <singhshivamkr@gmail.com>
2022-02-26 18:04:54 +0530

    :ambulance: Fix dynamic app theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,66 +1,66 @@
-import SideNav from './SideNav';
-import Workspace from './Workspace';
-import '@styles/app.scss';
-import Split from 'react-split';
-import SQLEditor from './Editor';
-import { useContext, useEffect, useRef } from 'react';
-import { AppContext } from '@contexts/AppContext';
-import { IGlobalState } from '~types/global';
+import SideNav from "./SideNav";
+import Workspace from "./Workspace";
+import "@styles/app.scss";
+import Split from "react-split";
+import SQLEditor from "./Editor";
+import { useCallback, useContext, useEffect, useRef } from "react";
+import { AppContext } from "@contexts/AppContext";
+import { IGlobalState } from "~types/global";
 
-const matchMedia = window.matchMedia('(prefers-color-scheme: dark)');
+const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
+
+const savedTheme = localStorage.getItem("theme") as IGlobalState["theme"];
 
 function App() {
   const { state, dispatch } = useContext(AppContext);
   const hasListener = useRef(false);
-  const setSystemTheme = (isDark: boolean) => {
+
+  const toggleDarkTheme = (isDark: boolean) => {
     if (isDark) {
-      return dispatch({type: 'switch_app_theme', appTheme: 'dark'});
+      dispatch({ type: "switch_app_theme", appTheme: "dark" });
+      return;
     }
-    dispatch({type: 'switch_app_theme', appTheme: 'light'})
+    dispatch({ type: "switch_app_theme", appTheme: "light" });
   };
+
+  const onThemeChange = useCallback(({ matches }: { matches: boolean }) => {
+    toggleDarkTheme(matches);
+  }, []);
 
   // listen to the theme state
   useEffect(() => {
+    localStorage.setItem("theme", state.theme);
+    // console.log("theme changed to " + state.theme);
 
-    const themeOnChange = ({ matches } : {matches: boolean}) => {
-      const savedTheme = localStorage.getItem('theme');
-      if(!savedTheme){
-        setSystemTheme(matches)
-      }
-    };
-    if (state.theme !== 'system') {
+    // if not system do nothing, we are all set
+    if (state.theme !== "system") {
+      toggleDarkTheme(state.theme === "dark");
+      if (!hasListener.current) return;
+      matchMedia.removeEventListener("change", onThemeChange);
+      // console.log("removed theme change listener");
+
+      hasListener.current = false;
       return;
     }
 
-    setSystemTheme(matchMedia.matches);
+    // toggle theme to match current system theme
+    toggleDarkTheme(matchMedia.matches);
 
     // only add listener if it doesn't exist
     if (hasListener.current) return;
     // listen for theme changes
-    matchMedia.addEventListener('change', themeOnChange);
-
+    matchMedia.addEventListener("change", onThemeChange);
     hasListener.current = true;
+    // console.log("added theme change listener");
   }, [state.theme]);
 
   useEffect(() => {
-    if(state.theme === "system") {
+    if (savedTheme) {
+      dispatch({ type: "switch_theme", theme: savedTheme });
       return;
     }
-    localStorage.setItem('theme', state.theme);
-    setSystemTheme(state.theme === "dark");
 
-  }, [state.theme]);
-
-
-  useEffect(() => {
-
-    const savedTheme = localStorage.getItem('theme') as IGlobalState['theme'] | null;
-    if(savedTheme) {
-      dispatch({type: 'switch_theme', theme: savedTheme})
-    } else {
-      setSystemTheme(matchMedia.matches);
-    }
-
+    toggleDarkTheme(matchMedia.matches);
   }, []);
 
   return (
@@ -81,5 +81,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
--- src/components/Workspace/Settings/index.tsx
+++ src/components/Workspace/Settings/index.tsx
@@ -1,24 +1,25 @@
-import { AppContext } from '@contexts/AppContext';
-import { useContext, useEffect, useState } from 'react';
-import type { IGlobalState } from '~types/global';
-import '@styles/Workspace/Settings/index.scss';
+import { AppContext } from "@contexts/AppContext";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
+import type { IGlobalState } from "~types/global";
+import "@styles/Workspace/Settings/index.scss";
 
 const Settings: React.FC = () => {
   const { state, dispatch } = useContext(AppContext);
+
+  // grab theme from localStorage, default to system
+  const savedTheme = localStorage.getItem("theme") ?? "system";
+
+  const themeSelectHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    const themeVal = e.target.value as IGlobalState["theme"];
+    dispatch({ type: "switch_theme", theme: themeVal });
+  };
 
   return (
     <div className="settings-container">
       <div className="theme-box">
         <p className="setting-name">Theme</p>
         <div className="theme-selector">
-          <select value={state.theme} onChange={(e) => {
-
-            const newTheme = (e.target.value as IGlobalState['theme']);
-            if(newTheme === "system") {
-              localStorage.removeItem('theme');
-            }
-            dispatch({type: 'switch_theme', theme: newTheme})
-          }}>
+          <select defaultValue={savedTheme} onChange={themeSelectHandler}>
             <option value="system">System</option>
             <option value="light">Light</option>
             <option value="dark">Dark</option>
